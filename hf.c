0d4b42548699fec8115337411d5e4e91bb067f9a
yerbol akhmetov <akhmetov_y@qwasar.io>
2022-06-30 11:55:09 +0000

    Save2

--- hf.c
+++ hf.c
@@ -4,6 +4,7 @@ char* my_strcpy(char* param_1, char* param_2) {
     int i = 0;
     while (param_2[i]!='\0') {
         param_1[i] = param_2[i];
+        i++;
     }
     return param_1;
 }
--- init.c
+++ init.c
@@ -1,7 +1,7 @@
 #include "source.h"
 
 t_game_data* init_struct() {
-    t_game_data* d = (t_game_data*) malloc(sizeof(t_game_data));
+    t_game_data* d = (t_game_data*)malloc(sizeof(t_game_data));
     d->secret_code = (char*) malloc(5);
     d->secret_code[4] = '\0';
     my_strcpy(d->secret_code, "1234");
